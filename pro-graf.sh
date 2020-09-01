8daef4f00736c0d0b2eda956627e98d7768be1d9
Faisal-97 <faisalabulnaja99@gmail.com>
2020-09-01 16:39:12 +0300

    updated ingress

--- pro-graf.sh
+++ pro-graf.sh
@@ -3,4 +3,4 @@ helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm repo update
 kubectl create namespace monitor
 helm install prometheus-operator stable/prometheus-operator --namespace monitor --set grafana.service.type=LoadBalancer
-kubectl apply -f ingress.yaml -n monitor
+kubectl apply -f ingress.yaml -f grafana.helm.yaml -n monitor
